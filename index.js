2c4f2577e8fb35acc93e0bdd8146ed848d8bf158
mindsgn <mindsgn@mindsgn.co.za>
2020-07-23 09:18:56 +0200

    port update

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(port, () => console.log('server is running', ${PORT}))
+server.listen(port, () => console.log('server is running', ${port}))

c7977d1490cca544574335bfd207d226a8592293
mindsgn <mindsgn@mindsgn.co.za>
2020-07-23 08:19:43 +0200

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-
 var port = process.env.PORT || 3000;
 const app = express();
 
-//const db = require('./models/index.js');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
@@ -15,22 +15,18 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/new', (req, res) => {
-  db.auth.findOrCreate({where: {phone:req.body.phone}})
-  .then(response => {
-    res.json({ success: true, message: "user registered" });
-    console.log(response)
-  }).catch(error => {
-    res.json({ success: false, message: "phone exist or is invalid" });
-    console.log(error)
-  });
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
+  socket.on('post', (message) => {
+    console.log('message: ', message);
+
+    io.emit('post', message);
+  });
 });
+
+server.listen(port, () => console.log('server is running'))
